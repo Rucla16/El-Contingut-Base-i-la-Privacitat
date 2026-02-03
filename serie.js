878e1839f13b52dc5bdb7e86f2a448a9c5a5b00b
Luca <lopritescu@institutmvm.cat>
2026-02-03 19:49:35 +0100

    Add files via upload

---
+++ serie.js
@@ -0,0 +1,22 @@
+import Multimedia from models
+class Serie {
+    constructor(capitols) {
+        this.capitols = capitols;
+    }
+    binge_watching(capitol) {
+        this.capitols = [];
+        capitol = 1;
+        for capitol in this.capitols;
+            capitol += capitol;
+            this.capitols.append(capitol);
+    }
+    play() {
+        console.log("Marató de", this.titol, "començada");
+        this.#views += this.capitols;
+    }
+    calcularRoyalties() {
+       if (this.capitols > 0);
+            this.#views * capitol * 0.05;
+        
+    }
+}
