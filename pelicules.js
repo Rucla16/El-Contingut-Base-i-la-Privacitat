31e758ea8a11961931c841f4600ca7a2d5d4f7f2
Luca <lopritescu@institutmvm.cat>
2026-02-03 21:15:31 +0100

    Refactor calcularRoyalties method and fix syntax

--- pelicules.js
+++ pelicules.js
@@ -1,18 +1,16 @@
-import Multimedia from models
+import Multimedia from models;
 class Pelicula {
     constructor(any, cost_fix) {
         this.any = any;
         this.cost_fix = cost_fix;
     }
-    play() {
-        console.log("Reproduint...");
-        this.#views + 1;
-    }
 
-    calcularRoyalties() {
-        if (this.cost_fix > 0);
-            this.#views * this.cost_fix;
-        
+    calcularRoyalties(royalties) {
+        let royalties = 0;
+        if (this.cost_fix > 0) {
+             royalties = this.#views * this.cost_fix;
+        }   
+         return royalties; 
     }
 
 }
